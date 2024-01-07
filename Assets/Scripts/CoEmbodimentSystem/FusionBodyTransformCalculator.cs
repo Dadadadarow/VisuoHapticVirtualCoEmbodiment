c94eb867b4002514a7080a64660c97feab6a5631
Dadadadarow <ground3101@gmail.com>
2024-01-07 18:03:34 +0900

    first commit

---
+++ Assets/Scripts/CoEmbodimentSystem/FusionBodyTransformCalculator.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CoEmbodimentSystem
+{
+    public class FusionBodyTransformCalculator : MonoBehaviour
+    {
+        private Transform _hostIkTarget;
+        private Transform _clientIkTarget;
+
+        [SerializeField] private FusionWeightController _fusionWeightController;
+
+        private Quaternion _previousRotation;
+        private Quaternion _currentRotation;
+        private Quaternion _currentReversedRotation;
+
+        private bool _isFirstFrame = true;
+
+        [SerializeField] private BodyPart _bodyPart;
+        public BodyPart ThisBodyPart { get { return _bodyPart; } }
+
+        private bool _isFusionStarted;
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (_fusionWeightController.gameObject.activeSelf)
+            {
+                if (_hostIkTarget != null && _clientIkTarget != null)
+                {
+                    CalculateTransform();
+                    if (_isFusionStarted)
+                    {
+                        return;
+                    }
+                    _isFusionStarted = true;
+                    this.GetComponent<WristCorrector>().StartCorrectingWrist();
+                }
+                else
+                {
+                    if (!_isFusionStarted)
+                    {
+                        return;
+                    }
+                    _isFusionStarted = false;
+                    this.GetComponent<WristCorrector>().StopCorrectingWrist();
+                }
+            }
+            else
+            {
+                if (_isFusionStarted)
+                {
+                    _isFusionStarted = false;
+                    this.GetComponent<WristCorrector>().StopCorrectingWrist();
+                }
+            }
+        }
+
+        public void SetIkTarget(Transform target, bool isHost)
+        {
+            if (isHost)
+            {
+                _hostIkTarget = target;
+            }
+            else
+            {
+                _clientIkTarget = target;
+            }
+        }
+
+        private void CalculateTransform()
+        {
+            int hostWeight = _fusionWeightController.HostWeightPercentage;
+            this.transform.position = (
+                _hostIkTarget.position * (float)hostWeight
+                + _clientIkTarget.position * (float)(100 - hostWeight)
+                ) / 100f;
+            
+            if (_bodyPart == BodyPart.LeftHand || _bodyPart == BodyPart.RightHand)
+            {
+                _currentRotation = Quaternion.Lerp(_clientIkTarget.rotation, _hostIkTarget.rotation, (float)(hostWeight / 100f));
+                _currentReversedRotation = Quaternion.AngleAxis(180, _currentRotation * this.transform.forward) * _currentRotation;
+
+                if (_isFirstFrame)
+                {
+                    _previousRotation = _currentRotation;
+                    _isFirstFrame = false;
+                    this.transform.rotation = _currentRotation;
+                }
+                else
+                {
+                    this.transform.rotation = GetNearestQuaternion(_currentRotation, _currentReversedRotation, _previousRotation);
+                }
+
+                _previousRotation = this.transform.rotation;
+            }
+        }
+
+        static public Quaternion GetNearestQuaternion(Quaternion rot1, Quaternion rot2, Quaternion refRot)
+        {
+            return (Mathf.Abs(Quaternion.Dot(rot1, refRot)) > Mathf.Abs(Quaternion.Dot(rot2, refRot))) ? rot1 : rot2;
+        }
+    }
+}
---
+++ Assets/Scripts/CoEmbodimentSystem/FusionWeightController.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Mirror;
+
+namespace CoEmbodimentSystem
+{
+    public class FusionWeightController : NetworkBehaviour
+    {
+        public enum ButtonInput
+        {
+            GuiButton,
+
+            InclimentButton,
+
+            DeclimentButton,
+        }
+
+        private Dictionary<ButtonInput, bool> _buttonInputs = new Dictionary<ButtonInput, bool>();
+
+        public int HostWeightPercentage { get; private set; }
+
+        private int _currentWeight;
+
+        private PlayerCommands _playerCommands;
+
+        [SerializeField] private int _defaultHostWeightPercentage;
+        [SerializeField] private GameObject _guiCanvas;
+        [SerializeField] private Text _selfWeightValue;
+
+        void Start()
+        {
+            HostWeightPercentage = _defaultHostWeightPercentage;
+            _buttonInputs.Add(ButtonInput.GuiButton, false);
+            _buttonInputs.Add(ButtonInput.InclimentButton, false);
+            _buttonInputs.Add(ButtonInput.DeclimentButton, false);
+
+            _guiCanvas.SetActive(false);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (isServer)
+            {
+                _currentWeight = HostWeightPercentage;
+            }
+            else
+            {
+                _currentWeight = 100 - HostWeightPercentage;
+            }
+
+            if (_buttonInputs[ButtonInput.GuiButton])
+            {
+                _guiCanvas.SetActive(true);
+
+                if (_buttonInputs[ButtonInput.InclimentButton])
+                {
+                    if (_currentWeight < 100)
+                    {
+                        _currentWeight++;
+                        if (isServer)
+                        {
+                            _playerCommands.CmdChangeHostWeight(_currentWeight);
+                        }
+                        else
+                        {
+                            _playerCommands.CmdChangeHostWeight(100 - _currentWeight);
+                        }
+                    }
+                }
+                if (_buttonInputs[ButtonInput.DeclimentButton])
+                {
+                    if (_currentWeight > 0)
+                    {
+                        _currentWeight--;
+                        if (isServer)
+                        {
+                            _playerCommands.CmdChangeHostWeight(_currentWeight);
+                        }
+                        else
+                        {
+                            _playerCommands.CmdChangeHostWeight(100 - _currentWeight);
+                        }
+                    }
+                }
+
+                _selfWeightValue.text = _currentWeight.ToString();
+            }
+            else
+            {
+                _guiCanvas.SetActive(false);
+            }
+        }
+
+        public void SetButtonInput(ButtonInput buttonInput, bool isPressed)
+        {
+            _buttonInputs[buttonInput] = isPressed;
+        }
+
+        public void ChangeHostWeight(int val)
+        {
+            HostWeightPercentage = val;
+        }
+
+        public void SetPlayerCommands(PlayerCommands playerCommands)
+        {
+            _playerCommands = playerCommands;
+        }
+    }
+}
