180466de8bd3d693dc68dae32f8a9704c9e045e0
Dadadadarow <ground3101@gmail.com>
2024-01-15 20:54:14 +0900

    bar sync

--- Assets/BarTransformSynchronizer.cs
+++ Assets/BarTransformSynchronizer.cs
@@ -5,24 +5,50 @@ using Mirror;
 
 public class BarTransformSynchronizer : NetworkBehaviour
 {
-    [SyncVar] public float height;
-    [SyncVar] public float tiltAngle;
+    [SyncVar] private float syncHeight;
+    [SyncVar] private float syncTiltAngle;
     [SerializeField] private Transform _synchronizeTarget_right;
     [SerializeField] private Transform _synchronizeTarget_left;
     private Quaternion initialRotation; // 初期回転
+
+    private float height;
+    private float tiltAngle;
+
     private void Start()
     {
         // 初期の高さと傾きを保存
-        initialRotation = this.transform.rotation;
+        initialRotation = transform.rotation;
     }
-    // Update is called once per frame
+
+    private void FixedUpdate()
+    {
+        TransmitTransform();
+    }
+
+    private void TransmitTransform()
+    {
+        if (isServer)
+        {
+            // サーバー側の処理
+            syncHeight = height;
+            syncTiltAngle = tiltAngle;
+        }
+        else if (isClient)
+        {
+            // クライアント側の処理
+            height = syncHeight;
+            tiltAngle = syncTiltAngle;
+        }
+    }
+
     void Update()
     {
         tiltAngle = -Mathf.Atan2(_synchronizeTarget_left.position.y - _synchronizeTarget_right.position.y, 1.1f) * Mathf.Rad2Deg;
         height = (_synchronizeTarget_left.position.y + _synchronizeTarget_right.position.y) / 2;
 
-        this.transform.position = new Vector3(0, height, this.transform.position.z);
         // 常にx座標は0
-        this.transform.rotation = Quaternion.Euler(0, 0, tiltAngle) * initialRotation;
+        Vector3 newPosition = new Vector3(0, height, 0);
+        transform.position = newPosition;
+        transform.rotation = Quaternion.Euler(0, 0, tiltAngle) * initialRotation;
     }
 }
